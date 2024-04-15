5404256af6cfd27edeabf8827e280c626d296fac
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-04-15 18:31:06 +0900

    Initial Commit

---
+++ devicemonitor/devicemonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  devicemonitorApp.swift
+//  devicemonitor
+//
+//  Created by Owner on 2024/04/15.
+//
+
+import SwiftUI
+
+@main
+struct devicemonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
